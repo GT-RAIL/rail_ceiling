60fc004ad8b4b981c4565ed517b179adc4ea430e
Steven Kordell <spkordell@gmail.com>
2014-06-26 12:09:32 -0400

    Layer plugin test

--- include/rail_ceiling/ceiling_layer.h
+++ include/rail_ceiling/ceiling_layer.h
@@ -1,5 +1,6 @@
 #ifndef CEILING_LAYER_H_
 #define CEILING_LAYER_H_
+
 #include <ros/ros.h>
 #include <costmap_2d/layer.h>
 #include <costmap_2d/layered_costmap.h>
@@ -8,21 +9,27 @@
 
 namespace ceiling_layer_namespace
 {
-  class CeilingLayer : public costmap_2d::Layer
+
+class CeilingLayer : public costmap_2d::Layer, public costmap_2d::Costmap2D
+{
+public:
+  CeilingLayer();
+
+  virtual void onInitialize();
+  virtual void updateBounds(double origin_x, double origin_y, double origin_yaw, double* min_x, double* min_y, double* max_x,
+                             double* max_y);
+  virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+  bool isDiscretized()
   {
-  public:
-    CeilingLayer();
+    return true;
+  }
 
-    virtual void onInitialize();
-    virtual void updateBounds(double origin_x, double origin_y, double origin_yaw, double* min_x, double* min_y, double* max_x,
-                               double* max_y);
-    virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+  virtual void matchSize();
 
-  private:
-    void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
-
-    double mark_x_, mark_y_;
-    dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
-  };
+private:
+  void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
+  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
+};
 }
+
 #endif //CEILING_LAYER_H_
--- src/ceiling_layer.cpp
+++ src/ceiling_layer.cpp
@@ -4,6 +4,7 @@
 PLUGINLIB_EXPORT_CLASS(ceiling_layer_namespace::CeilingLayer, costmap_2d::Layer)
 
 using costmap_2d::LETHAL_OBSTACLE;
+using costmap_2d::NO_INFORMATION;
 
 namespace ceiling_layer_namespace
 {
@@ -14,12 +15,23 @@ void CeilingLayer::onInitialize()
 {
   ros::NodeHandle nh("~/" + name_);
   current_ = true;
+  default_value_ = NO_INFORMATION;
+  matchSize();
 
   dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
   dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(
       &CeilingLayer::reconfigureCB, this, _1, _2);
   dsrv_->setCallback(cb);
 }
+
+
+void CeilingLayer::matchSize()
+{
+  Costmap2D* master = layered_costmap_->getCostmap();
+  resizeMap(master->getSizeInCellsX(), master->getSizeInCellsY(), master->getResolution(),
+            master->getOriginX(), master->getOriginY());
+}
+
 
 void CeilingLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
 {
@@ -32,13 +44,17 @@ void CeilingLayer::updateBounds(double origin_x, double origin_y, double origin_
   if (!enabled_)
     return;
 
-  mark_x_ = origin_x + cos(origin_yaw);
-  mark_y_ = origin_y + sin(origin_yaw);
+  double mark_x = origin_x + cos(origin_yaw), mark_y = origin_y + sin(origin_yaw);
+  unsigned int mx;
+  unsigned int my;
+  if(worldToMap(mark_x, mark_y, mx, my)){
+    setCost(mx, my, LETHAL_OBSTACLE);
+  }
 
-  *min_x = std::min(*min_x, mark_x_);
-  *min_y = std::min(*min_y, mark_y_);
-  *max_x = std::max(*max_x, mark_x_);
-  *max_y = std::max(*max_y, mark_y_);
+  *min_x = std::min(*min_x, mark_x);
+  *min_y = std::min(*min_y, mark_y);
+  *max_x = std::max(*max_x, mark_x);
+  *max_y = std::max(*max_y, mark_y);
 }
 
 void CeilingLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
@@ -46,10 +62,16 @@ void CeilingLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, in
 {
   if (!enabled_)
     return;
-  unsigned int mx;
-  unsigned int my;
-  if(master_grid.worldToMap(mark_x_, mark_y_, mx, my)){
-    master_grid.setCost(mx, my, LETHAL_OBSTACLE);
+
+  for (int j = min_j; j < max_j; j++)
+  {
+    for (int i = min_i; i < max_i; i++)
+    {
+      int index = getIndex(i, j);
+      if (costmap_[index] == NO_INFORMATION)
+        continue;
+      master_grid.setCost(i, j, costmap_[index]);
+    }
   }
 }
 
