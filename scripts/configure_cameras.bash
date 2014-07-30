b5c07daed80d20c3ba3f207ad51937a04791ee45
Steven Kordell <spkordell@gmail.com>
2014-07-30 12:57:56 -0400

    Added bundle for black table

--- scripts/configure_cameras.bash
+++ scripts/configure_cameras.bash
@@ -8,3 +8,11 @@ v4l2-ctl --device=/dev/video1  --set-ctrl=exposure_absolute=50
 
 v4l2-ctl --device=/dev/video2 --set-ctrl=exposure_auto=1
 v4l2-ctl --device=/dev/video2  --set-ctrl=exposure_absolute=50
+
+#TODO: Uncomment the below when connecting the rest of the cameras
+
+#v4l2-ctl --device=/dev/video3 --set-ctrl=exposure_auto=1
+#v4l2-ctl --device=/dev/video3  --set-ctrl=exposure_absolute=50
+
+#v4l2-ctl --device=/dev/video4 --set-ctrl=exposure_auto=1
+#v4l2-ctl --device=/dev/video4  --set-ctrl=exposure_absolute=50
